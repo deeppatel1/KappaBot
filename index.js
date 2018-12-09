2c2db2ff4e34860d7cbf9850a3363cf7dba963a6
Deep <pateldeep1996@gmail.com>
2018-12-09 13:36:56 -0500

    != to ==

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.on('ready', () => {
 
     Twitterclient.stream('statuses/filter', {follow: '4833803780,736784706486734852,344538810,873949601522487297'},  function(stream) {
         stream.on('data', function(tweet) {
-            if ((tweet.user.screen_name != 'loltyler1') || (tweet.user.screen_name != 'REALIcePoseidon') || (tweet.user.screen_name != 'TLDoublelift') || (tweet.user.screen_name != 'JacobK_Cx')){
+            if ((tweet.user.screen_name == 'loltyler1') || (tweet.user.screen_name == 'REALIcePoseidon') || (tweet.user.screen_name == 'TLDoublelift') || (tweet.user.screen_name == 'JacobK_Cx')){
                 client.channels.get("173611297387184129").send("<@173611085671170048> <@173610714433454084> https://twitter.com/" + tweet.user.screen_name +"/status/" + tweet.id_str);
             }
         });
