81ceeb3870c8b6f85ef5d411d8e4281077a33b77
Deep <pateldeep1996@gmail.com>
2019-05-10 01:11:49 -0400

    aklsdjflasjfk

--- twitchLiveAndPost.js
+++ twitchLiveAndPost.js
@@ -78,7 +78,7 @@ function pollToCheckTwitcherIsLive(TWITCHer, clientfordiscord){
                                 + currentdate.getMinutes() + ":"
                                 + currentdate.getSeconds();
                     
-                    var sql_query = 'INSERT INTO cxnetwork (date, url, name, time) SELECT \'' + datetime +'\', \'' + dateStreamStarted + '\', \'' + "Twitch" + '\', \'' + time + '\' WHERE NOT EXISTS (SELECT 1 FROM cxnetwork WHERE url=\''+ url +'\');'                    
+                    var sql_query = 'INSERT INTO cxnetwork (date, url, name, time) SELECT \'' + datetime +'\', \'' + dateStreamStarted + '\', \'' + "Twitch" + '\', \'' + time + '\' WHERE NOT EXISTS (SELECT 1 FROM cxnetwork WHERE url=\''+ 'twitch.tv/loltyler1' +'\');'                    
                     dbQuery.query(sql_query);
                     var messageToPost = twitchStreamer[TWITCHer] + ' is LIVE ' + twitchStreamer[TWITCHer]['URL'];
                     discordPost.postToDiscord(clientForDiscord, TWITCHer, messageToPost, false);
