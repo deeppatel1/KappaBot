5fa2a1f76ec9dc666eab06da667bbe28464fa8a8
deeppatel1 <pateldeep1996@gmail.com>
2019-07-14 14:47:50 -0400

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -62,9 +62,7 @@ var twitterClientSubscribeToListDelimittedComma = '';
 
 for (tweeter in twitterAccounts){
     twitterClientSubscribeToListDelimittedComma = twitterClientSubscribeToListDelimittedComma + twitterAccounts[tweeter].twitterId;
-    console.log(tweeter);
-    console.log(twitterAccounts.length);
-    if (tweeter != twitterAccounts.length - 1) twitterClientSubscribeToListDelimittedComma = twitterClientSubscribeToListDelimittedComma + ',';
+    if (tweeter != 'lcs') twitterClientSubscribeToListDelimittedComma = twitterClientSubscribeToListDelimittedComma + ',';
 }
 
 console.log(twitterClientSubscribeToListDelimittedComma);
