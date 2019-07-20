43c6cc545c63646d7fb305a7c2582661ba77b188
Deep <pateldeep1996@gmail.com>
2019-07-20 18:06:50 -0400

    twitch post should now work

--- twitchLiveAndPost.js
+++ twitchLiveAndPost.js
@@ -124,7 +124,7 @@ function pollToCheckTwitcherIsLive(TWITCHer, clientfordiscord){
                     var sql_query = 'INSERT INTO cxnetwork (date, url, name, time) SELECT \'' + datetime +'\', \'' + dateStreamStarted + '\', \'' + "Twitch" + '\', \'' + time + '\' WHERE NOT EXISTS (SELECT 1 FROM cxnetwork WHERE url=\''+ 'twitch.tv/loltyler1' +'\');'                    
                     dbQuery.query(sql_query);
                     var messageToPost = twitchStreamer[TWITCHer] + ' is LIVE ' + twitchStreamer[TWITCHer]['URL'];
-                    discordPost.postToDiscord(clientForDiscord, '', messageToPost, false, "main-channel");
+                    discordPost.postToDiscord(clientfordiscord, '', messageToPost, false, "main-channel");
                 }                
             })
         }else{
