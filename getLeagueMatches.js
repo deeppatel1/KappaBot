a8e2bf47e4e6f225d0e6f67e50b21c5effcf15d0
Deep <pateldeep1996@gmail.com>
2019-07-25 22:13:18 -0400

    asdfasdf

--- getLeagueMatches.js
+++ getLeagueMatches.js
@@ -83,7 +83,7 @@ module.exports = {
 				console.log(returnArrayOfMatches);
 				for (var a = 0; (a < returnArrayOfMatches.length) && (counter < 5); a++) {
 
-					if ((returnArrayOfMatches[a]['league'] == 'cblol 2019 1st split') || (returnArrayOfMatches[a]['league'] == '2019 LMS Spring Split') || (returnArrayOfMatches[a]['league'] == 'LJL 2019 Spring Split') || (returnArrayOfMatches[a]['league'] == '2019 LMS Spring Split')) {
+					if ((returnArrayOfMatches[a]['league'].contains('cblol')) || (returnArrayOfMatches[a]['league'].contains('LMS')) || (returnArrayOfMatches[a]['league'].contains('LJL')) || (returnArrayOfMatches[a]['league'].contains('LMS')) || (returnArrayOfMatches[a]['league'].contains('OPL'))) {
 						//a = a - 1;
 					} else {
 
