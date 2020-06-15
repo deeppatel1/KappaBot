b1fb85fba4053fed626bded28ecb2c246f7b805e
deeppatel1 <pateldeep1996@gmail.com>
2020-06-15 19:26:02 -0400

    Update discordPost.js

--- discordPost.js
+++ discordPost.js
@@ -9,24 +9,21 @@ module.exports = {
         //console.log("---YTeR:" + YTer + " msgToPost: " + msgToPost + " channel: " + channel + " posting channel: " + postingChannel);
         if ((YTer == 'Twitch') || (YTer == '')) {
             //var channel = "173611297387184129;
-            clientForDiscord.channels.get('173611297387184129').send(msgToPost);
+            clientForDiscord.channels.get('284157566693539851').send(msgToPost);
             return;
         }
         //var discordChannel = (streamersTracker[YTer].discordChannelToPost == "main") ? "173611297387184129" : "284157566693539851"
 
         //console.log(clientForDiscord.channel);
 
-        if (msgToPost.includes("twitter")){
-            clientForDiscord.channels.get('284157566693539851').send(msgToPost)
-        }
-
+        
         if (!ifEmbed){
             // main discord channel is          173611297387184129
             // secondary discord channel is     284157566693539851
             console.log("[" + YTer + "] " + "Now posting  ---- " + msgToPost + "  " + new Date())
-            clientForDiscord.channels.get('173611297387184129').send(msgToPost)
+            clientForDiscord.channels.get('284157566693539851').send(msgToPost)
         }else{
-            clientForDiscord.channels.get('173611297387184129').send(msgToPost)
+            clientForDiscord.channels.get('284157566693539851').send(msgToPost)
         }
 
     }
