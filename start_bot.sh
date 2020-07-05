051d50c12a6e0c3ff27b3331544e0bdbba5e9728
Deep <pateldeep1996@gmail.com>
2020-07-05 19:48:34 -0400

    aa

--- start_bot.sh
+++ start_bot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash/
-cd ~/.forever/
+cd /home/kapp/.forever/
 rm kappabot.log
 touch kappabot.log
 sleep 8
-cd ~/KappaBot/KappaBot/
+cd /home/kapp/KappaBot/KappaBot/
 git pull --all
-python3 python_discord.py
-forever start -a -l kappabot.log index.js -p 8000
-python3 -m pip install -r requirements.txt
+forever start -a -l /home/kapp/.forever/kappabot.log index.js -p 8000
+pip3 install --upgrade
+pip3 install -r requirements.txt
+forever start -a -l /home/kapp/.forever/kappabot.log -c python3 python_discord.py
