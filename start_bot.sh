4906db4c28f2236c2ce3675becc33fdba2127fb1
Deep <pateldeep1996@gmail.com>
2020-07-05 19:18:01 -0400

    test

--- start_bot.sh
+++ start_bot.sh
@@ -6,6 +6,5 @@ sleep 8
 cd ~/KappaBot/KappaBot/
 git pull --all
 forever start -a -l kappabot.log index.js -p 8000
-pip3 install --upgrade
-pip3 install -r requirements.txt
+python3 -m pip install -r requirements.txt
 forever start -l kappabot.log -c python3 python_discord.py
