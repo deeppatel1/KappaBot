34da9ad34feba5445835350d77a24517d3102a90
Deep <pateldeep1996@gmail.com>
2020-07-05 19:24:11 -0400

    daf

--- start_bot.sh
+++ start_bot.sh
@@ -5,6 +5,6 @@ touch kappabot.log
 sleep 8
 cd ~/KappaBot/KappaBot/
 git pull --all
+python3 python_discord.py
 forever start -a -l kappabot.log index.js -p 8000
 python3 -m pip install -r requirements.txt
-forever start -l kappabot.log -c python3 python_discord.py
